0882df3b1b8b10616d68f34d9a7c8cb1f7c4bc99
iceberg20 <italoberg@yahoo.com.br>
2016-05-20 22:11:35 -0300

    Desabling button to don't interrup the animation

--- app/assets/javascripts/animation.js
+++ app/assets/javascripts/animation.js
@@ -2,9 +2,11 @@ function myMove() {
   var elem = document.getElementById("animate");   
   var pos = 0;
   var id = setInterval(frame, 5);
+  document.getElementById("btn").disabled = true;
   function frame() {
     if (pos == 350) {
       clearInterval(id);
+      document.getElementById("btn").disabled = false;
     } else {
       pos++; 
       elem.style.top = pos + 'px'; 
