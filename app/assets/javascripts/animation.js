fc3bebec5f053af05dd5a6598117a5a087ac7837
iceberg20 <italoberg@yahoo.com.br>
2016-05-21 11:48:28 -0300

    Progressive bar

--- app/assets/javascripts/animation.js
+++ app/assets/javascripts/animation.js
@@ -14,3 +14,17 @@ function myMove() {
     }
   }
 }
+
+function move() {
+    var elem = document.getElementById("myBar"); 
+    var width = 2;
+    var id = setInterval(frame, 10);
+    function frame() {
+        if (width >= 100) {
+            clearInterval(id);
+        } else {
+            width++; 
+            elem.style.width = width + '%'; 
+        }
+    }
+}
